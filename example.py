30dbb12f4f8cc63a967fdffd18f562e3b12a57c3
nnitz <163340091+nnitz@users.noreply.github.com>
2025-07-07 23:01:44 +0200

    example

--- example.py
+++ example.py
@@ -1,13 +1,9 @@
 import streamlit as st
 from openai import OpenAI
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+st.title("💬 Natalie's RAG")
 
-st.title("💬 Chatbot")
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
